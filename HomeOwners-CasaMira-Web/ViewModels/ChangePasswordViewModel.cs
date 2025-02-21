3a19b87259f8bbcc19ab0c787e0e403bb5a6abe3
Remart30 <remartybanez30@gmail.com?>
2025-02-22 00:57:28 +0800

    Add project files.

---
+++ HomeOwners-CasaMira-Web/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeOwners_CasaMira_Web.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+         [Required(ErrorMessage = "Email i required. ")]
+        [EmailAddress]
+
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(40, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = " New Password")]
+        [Compare("Confirm New Password", ErrorMessage = "Password does not match")]
+        public string NewPassword { get; set; }
+
+
+        [Required(ErrorMessage = "Confirm Password is Required. ")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        public string ConfirmNewPassword { get; set; }
+
+    }
+}
---
+++ HomeOwners-CasaMira-Web/ViewModels/VerifyEmailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeOwners_CasaMira_Web.ViewModels
+{
+    public class VerifyEmailViewModel
+    {
+
+        [Required(ErrorMessage = "Email i required. ")]
+        [EmailAddress]
+
+        public string Email { get; set; }
+    }
+}
