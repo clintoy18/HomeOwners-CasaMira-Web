eaa2f14635982d6a0c061615a8586d6c65bfbbec
clintoy18 <remartybanez30@gmail.com?>
2025-02-22 01:44:16 +0800

    Restore deleted Files

---
+++ HomeOwners-CasaMira-Web/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeOwners_CasaMira_Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ HomeOwners-CasaMira-Web/Models/Users.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HomeOwners_CasaMira_Web.Models
+{
+    public class Users : IdentityUser
+    {
+        public int FullName { get; set; }
+    }
+}
