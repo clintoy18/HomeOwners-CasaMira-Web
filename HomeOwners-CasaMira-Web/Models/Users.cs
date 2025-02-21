3f3c162ffd07d9dd5e77c583657fe00466f37044
clintoy18 <remartybanez30@gmail.com?>
2025-02-22 01:06:09 +0800

    Revert "Add project files."
    This reverts commit 3a19b87259f8bbcc19ab0c787e0e403bb5a6abe3.

--- HomeOwners-CasaMira-Web/Models/Users.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace HomeOwners_CasaMira_Web.Models
-{
-    public class Users : IdentityUser
-    {
-        public int FullName { get; set; }
-    }
-}
