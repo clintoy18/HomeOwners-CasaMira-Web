59b39a551ecb23aa6e10243b237f4549b14c52d2
clintoy18 <aicsalonzo@gmail.com>
2025-04-25 23:27:44 +0800

    final migration fix

--- HomeOwners-CasaMira-Web/Data/AppDbContext.cs
+++ HomeOwners-CasaMira-Web/Data/AppDbContext.cs
@@ -8,12 +8,38 @@ namespace HomeOwners_CasaMira_Web.Data
 {
     public class AppDbContext : IdentityDbContext<Users>
     {
-        public AppDbContext(DbContextOptions options) : base(options){ }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Announcement> Announcements { get; set; }
         public DbSet<FacilityReservation> FacilityReservations { get; set; }
         public DbSet<ServiceRequest> ServiceRequests { get; set; }
         public DbSet<Document> Documents { get; set; }
         public DbSet<Facility> Facilities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure ServiceRequest entity
+            modelBuilder.Entity<ServiceRequest>()
+                .Property(s => s.Description)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            modelBuilder.Entity<ServiceRequest>()
+                .Property(s => s.Location)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<ServiceRequest>()
+                .Property(s => s.StaffNotes)
+                .HasMaxLength(500)
+                .IsRequired(false);
+
+            modelBuilder.Entity<ServiceRequest>()
+                .Property(s => s.Status)
+                .IsRequired()
+                .HasDefaultValue("Pending");
+        }
 
         public static implicit operator AppDbContext(AppbContext v)
         {
--- HomeOwners-CasaMira-Web/Models/ServiceRequest.cs
+++ HomeOwners-CasaMira-Web/Models/ServiceRequest.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HomeOwners_CasaMira_Web.Models
 {
     public class ServiceRequest
     {
+        [Key]
         public int Id { get; set; }
 
         [Required]
@@ -16,6 +18,7 @@ namespace HomeOwners_CasaMira_Web.Models
 
         [Required]
         [StringLength(500)]
+        [Display(Name = "Description")]
         public string Description { get; set; }
 
         [Required]
